e2cb3a4861f4f954a400b20b7fd98f759624b156
PatHoo <quith@qq.com>
2022-12-30 18:15:42 +0800

    .

--- Prod/backend.tf
+++ Prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
-    # Change string `prd` to `prd.tfvars.env_name`
-    bucket = "tsp-prd-bucket"
-    key="tsp/prd.tfstate"
-  }
+  # backend "s3" {
+  #   # Change string `prd` to `prd.tfvars.env_name`
+  #   bucket = "tsp-prd-bucket"
+  #   key="tsp/prd.tfstate"
+  # }
 }
